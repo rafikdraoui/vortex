28ec3dca828f7808df8910a32ff928c2b45e03fd
Rafik Draoui <rafik@rafik.ca>
2012-03-13 02:43:26 -0400

    Basic views and templates

--- musique/views.py
+++ musique/views.py
@@ -1,1 +1,26 @@
-# Create your views here.
+from django.shortcuts import render_to_response
+from django.views.generic import DetailView
+
+from models import Artist, Album, Song
+
+
+class ArtistDetailView(DetailView):
+    model = Artist
+
+    def get_context_data(self, **kwargs):
+        context = super(ArtistDetailView, self).get_context_data(**kwargs)
+        context['albums'] = context['object'].album_set.all()
+        return context
+
+
+class AlbumDetailView(DetailView):
+    model = Album
+
+    def get_context_data(self, **kwargs):
+        context = super(AlbumDetailView, self).get_context_data(**kwargs)
+        context['songs'] = context['object'].song_set.all()
+        return context
+
+
+def home(request):
+    return render_to_response('musique/home.html')
--- urls.py
+++ urls.py
@@ -1,17 +1,28 @@
 from django.conf.urls.defaults import patterns, include, url
+from django.views.generic import ListView, DetailView
+
+from musique.models import Artist, Album, Song
+from musique.views import ArtistDetailView, AlbumDetailView
+
+from musique import utils, views
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'vortex.views.home', name='home'),
-    # url(r'^vortex/', include('vortex.foo.urls')),
+    url(r'^$', views.home),
 
+    url(r'^admin/', include(admin.site.urls)),
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^artist/$', ListView.as_view(model=Artist)),
+    url(r'^artist/(?P<pk>\d+)/$', ArtistDetailView.as_view()),
+
+    url(r'^album/$', ListView.as_view(model=Album)),
+    url(r'^album/(?P<pk>\d+)/$', AlbumDetailView.as_view()),
+
+    url(r'^song/$', ListView.as_view(model=Song)),
+    url(r'^song/(?P<pk>\d+)$', DetailView.as_view(model=Song)),
 )

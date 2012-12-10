c44cdc0d1a155832765e7e036e491f4c25617633
Rafik Draoui <rafik@rafik.ca>
2012-12-09 19:02:17 -0500

    Removed custom DetailView for Artist and Album models.

--- vortex/library/views.py
+++ vortex/library/views.py
@@ -7,7 +7,7 @@ from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.views import defaults
 from django.views.decorators.csrf import requires_csrf_token
-from django.views.generic import DetailView, ListView
+from django.views.generic import ListView
 from django.utils.encoding import iri_to_uri
 from django.utils.http import urlquote
 from django.utils.translation import ugettext_lazy as _
@@ -21,25 +21,6 @@ class AlphabetizedListView(ListView):
     def get_context_data(self, **kwargs):
         context = super(AlphabetizedListView, self).get_context_data(**kwargs)
         context['alpha_list'] = get_alphabetized_list(self.model)
-        return context
-
-
-class ArtistDetailView(DetailView):
-    model = Artist
-
-    def get_context_data(self, **kwargs):
-        context = super(ArtistDetailView, self).get_context_data(**kwargs)
-        context['albums'] = context['object'].album_set.all()
-        return context
-
-
-class AlbumDetailView(DetailView):
-    model = Album
-    queryset = Album.objects.select_related('artist')
-
-    def get_context_data(self, **kwargs):
-        context = super(AlbumDetailView, self).get_context_data(**kwargs)
-        context['songs'] = context['object'].song_set.all()
         return context
 
 
